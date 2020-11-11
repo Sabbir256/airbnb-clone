716d550efa06b73c048f3c412749182d702ae230
aboutsabbir <sabbir.ruet15@gmail.com>
2020-11-11 12:54:30 +0600

    sec comm

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="footer">
-            <p>&#169; 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+            <p>&#169; 2020 Airbnb clone by Sabbir Ahmed Badhon! No rights reserved - this is a demo!</p>
             <p>Privacy &middot; Terms &middot; Sitemap &middot; Company Details</p>
         </div>
     )
